ea98834cbae1b10a182eec28aedbbe3016d2b8c7
Masood Fallahpoor <masood.fallahpoor@gmail.com>
2018-08-16 14:30:20 +0430

    Display venues based on user's last known location (for the time being)

--- app/src/main/java/ir/fallahpoor/vicinity/venues/view/VenuesActivity.java
+++ app/src/main/java/ir/fallahpoor/vicinity/venues/view/VenuesActivity.java
@@ -1,18 +1,31 @@
 package ir.fallahpoor.vicinity.venues.view;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.hannesdorfmann.mosby3.mvp.MvpActivity;
 
 import java.util.List;
 
+import ir.fallahpoor.vicinity.BuildConfig;
 import ir.fallahpoor.vicinity.R;
 import ir.fallahpoor.vicinity.UiThread;
 import ir.fallahpoor.vicinity.data.WebServiceFactory;
@@ -28,11 +41,16 @@ import ir.fallahpoor.vicinity.venues.presenter.VenuesPresenterImpl;
 
 public class VenuesActivity extends MvpActivity<VenuesView, VenuesPresenter> implements VenuesView {
 
+    private static final int REQUEST_CODE_ACCESS_FINE_LOCATION = 1000;
+    private static final String TAG = "@@@@@@";
+
     private RecyclerView venuesRecyclerView;
     private RelativeLayout tryAgainLayout;
     private RelativeLayout loadingLayout;
     private TextView errorMessageTextView;
     private Button tryAgainButton;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private Location lastLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +60,7 @@ public class VenuesActivity extends MvpActivity<VenuesView, VenuesPresenter> imp
 
         bindViews();
 
-        getPresenter().getVenuesAround(35.734410, 51.394560);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
     }
 
@@ -52,6 +70,69 @@ public class VenuesActivity extends MvpActivity<VenuesView, VenuesPresenter> imp
         loadingLayout = findViewById(R.id.loading_layout);
         errorMessageTextView = findViewById(R.id.error_message_text_view);
         tryAgainButton = findViewById(R.id.try_again_button);
+    }
+
+    @Override
+    public void onStart() {
+
+        super.onStart();
+
+        if (isAccessFineLocationPermissionGranted()) {
+            getLastLocation();
+        } else {
+            requestPermission();
+        }
+
+    }
+
+    private boolean isAccessFineLocationPermissionGranted() {
+        int permissionState = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        return (permissionState == PackageManager.PERMISSION_GRANTED);
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getLastLocation() {
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, location -> {
+                    if (location != null) {
+                        this.lastLocation = location;
+                        Log.d(TAG, "Location: " + location.getLatitude() + "," + location.getLongitude());
+                        getPresenter().getVenuesAround(location.getLatitude(), location.getLongitude());
+                    } else {
+                        Snackbar.make(findViewById(android.R.id.content), R.string.location_disabled, Snackbar.LENGTH_INDEFINITE)
+                                .show();
+                    }
+                });
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                REQUEST_CODE_ACCESS_FINE_LOCATION);
+    }
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if (requestCode == REQUEST_CODE_ACCESS_FINE_LOCATION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d(TAG, "Permission granted.");
+                getLastLocation();
+            } else {
+                // Permission is denied either temporarily or permanently.
+                Snackbar.make(findViewById(android.R.id.content), R.string.permission_denied_explanation, Snackbar.LENGTH_INDEFINITE)
+                        .setAction(R.string.settings, view -> launchAppSettings())
+                        .show();
+            }
+        }
+
+    }
+
+    private void launchAppSettings() {
+        Intent intent = new Intent();
+        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Uri uri = Uri.fromParts("package", BuildConfig.APPLICATION_ID, null);
+        intent.setData(uri);
+        startActivity(intent);
     }
 
     @NonNull
@@ -76,7 +157,10 @@ public class VenuesActivity extends MvpActivity<VenuesView, VenuesPresenter> imp
     @Override
     public void showError(String errorMessage) {
         errorMessageTextView.setText(errorMessage);
-        tryAgainButton.setOnClickListener(view -> getPresenter().getVenuesAround(35.734410, 51.394560));
+        tryAgainButton.setOnClickListener(view -> {
+            if (lastLocation != null)
+                getPresenter().getVenuesAround(lastLocation.getLatitude(), lastLocation.getLongitude());
+        });
         tryAgainLayout.setVisibility(View.VISIBLE);
         venuesRecyclerView.setVisibility(View.GONE);
     }
