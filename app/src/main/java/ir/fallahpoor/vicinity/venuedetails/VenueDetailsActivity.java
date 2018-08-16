b83dd1adbd1516b760a8568c9e4fd4709d71699e
Masood Fallahpoor <masood.fallahpoor@gmail.com>
2018-08-16 11:48:37 +0430

    Implement the logic of VenueDetailsPresenterImpl

--- app/src/main/java/ir/fallahpoor/vicinity/venuedetails/VenueDetailsActivity.java
+++ app/src/main/java/ir/fallahpoor/vicinity/venuedetails/VenueDetailsActivity.java
@@ -6,6 +6,14 @@ import android.support.annotation.NonNull;
 import com.hannesdorfmann.mosby3.mvp.MvpActivity;
 
 import ir.fallahpoor.vicinity.R;
+import ir.fallahpoor.vicinity.UiThread;
+import ir.fallahpoor.vicinity.data.WebServiceFactory;
+import ir.fallahpoor.vicinity.data.executor.JobExecutor;
+import ir.fallahpoor.vicinity.data.mapper.VenuesEntityDataMapper;
+import ir.fallahpoor.vicinity.data.repository.VenuesRepositoryImpl;
+import ir.fallahpoor.vicinity.domain.interactors.GetVenueDetailsUseCase;
+import ir.fallahpoor.vicinity.domain.repository.VenuesRepository;
+import ir.fallahpoor.vicinity.venuedetails.model.VenueDetailsDataMapper;
 import ir.fallahpoor.vicinity.venuedetails.presenter.VenueDetailsPresenter;
 import ir.fallahpoor.vicinity.venuedetails.presenter.VenueDetailsPresenterImpl;
 import ir.fallahpoor.vicinity.venuedetails.view.VenueDetailsView;
@@ -22,7 +30,9 @@ public class VenueDetailsActivity extends MvpActivity<VenueDetailsView, VenueDet
     @NonNull
     @Override
     public VenueDetailsPresenter createPresenter() {
-        return new VenueDetailsPresenterImpl();
+        VenuesRepository venuesRepository = new VenuesRepositoryImpl(new WebServiceFactory(), new VenuesEntityDataMapper());
+        GetVenueDetailsUseCase getPlaceDetailsUseCase = new GetVenueDetailsUseCase(venuesRepository, new JobExecutor(), new UiThread());
+        return new VenueDetailsPresenterImpl(getPlaceDetailsUseCase, new VenueDetailsDataMapper());
     }
 
     @Override
--- app/src/main/java/ir/fallahpoor/vicinity/venuedetails/presenter/VenueDetailsPresenterImpl.java
+++ app/src/main/java/ir/fallahpoor/vicinity/venuedetails/presenter/VenueDetailsPresenterImpl.java
@@ -2,12 +2,46 @@ package ir.fallahpoor.vicinity.venuedetails.presenter;
 
 import com.hannesdorfmann.mosby3.mvp.MvpBasePresenter;
 
+import io.reactivex.disposables.Disposable;
+import ir.fallahpoor.vicinity.domain.interactors.GetVenueDetailsUseCase;
+import ir.fallahpoor.vicinity.venuedetails.model.VenueDetailsDataMapper;
 import ir.fallahpoor.vicinity.venuedetails.view.VenueDetailsView;
 
 public class VenueDetailsPresenterImpl extends MvpBasePresenter<VenueDetailsView> implements VenueDetailsPresenter {
 
+    private GetVenueDetailsUseCase getVenueDetailsUseCase;
+    private VenueDetailsDataMapper venueDetailsDataMapper;
+    private Disposable disposable;
+
+    public VenueDetailsPresenterImpl(GetVenueDetailsUseCase getVenueDetailsUseCase, VenueDetailsDataMapper venueDetailsDataMapper) {
+        this.getVenueDetailsUseCase = getVenueDetailsUseCase;
+        this.venueDetailsDataMapper = venueDetailsDataMapper;
+    }
+
     @Override
     public void getVenueDetails(String id) {
+
+        ifViewAttached(VenueDetailsView::showLoading);
+
+        disposable = getVenueDetailsUseCase.execute(GetVenueDetailsUseCase.Params.forVenue(id))
+                .subscribe(
+                        places -> ifViewAttached(view -> {
+                            view.hideLoading();
+                            view.showPlace(venueDetailsDataMapper.transform(places));
+                        }),
+                        throwable -> ifViewAttached(view -> {
+                            view.hideLoading();
+                            view.showError(throwable.getMessage());
+                        }));
+
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        if (disposable != null && !disposable.isDisposed()) {
+            disposable.dispose();
+        }
     }
 
 }
