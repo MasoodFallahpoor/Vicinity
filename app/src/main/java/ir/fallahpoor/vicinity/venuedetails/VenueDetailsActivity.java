46b8acb72f836deae36aede706563cfd14405294
Masood Fallahpoor <masood.fallahpoor@gmail.com>
2018-08-16 11:16:37 +0430

    Add an Activity to display details of selected venue

---
+++ app/src/main/java/ir/fallahpoor/vicinity/venuedetails/VenueDetailsActivity.java
@@ -0,0 +1,16 @@
+package ir.fallahpoor.vicinity.venuedetails;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import ir.fallahpoor.vicinity.R;
+
+public class VenueDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_venue_details);
+    }
+
+}
